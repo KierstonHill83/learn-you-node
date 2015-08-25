88d1d688ac3efae7b8aac94b01cd4ef53734359a
KierstonHill83 <khill1218@gmail.com>
2015-08-25 10:40:43 -0600

    Finished the first four exercises

---
+++ node.js
@@ -0,0 +1,34 @@
+//problem 1
+// console.log("HELLO WORLD");
+
+
+//problem 2
+// console.log(sum(process.argv));
+// function sum(arr) {
+//   var total = 0;
+//   for (var i = 2; i < arr.length; i++) {
+//     total += +arr[i];
+//   }
+//   return total;
+// }
+
+
+//problem 3
+// var fs = require('fs');
+// var path = process.argv[2];
+// var lines = fs.readFileSync(path, 'utf8');
+// console.log(lines.split('\n').length-1);
+
+
+//problem 4
+// var fs = require('fs');
+// var path = process.argv[2];
+
+// fs.readFile(path, 'utf8', function(err, data){
+//   if (err) throw err;
+//   var lines = data.split('\n').length-1;
+//   console.log(lines);
+// });
+
+
+//problem 5
